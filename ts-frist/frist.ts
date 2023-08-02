eef9db6e1173e9ee7ba2c24751b04204a5584a84
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-08-02 21:02:01 +0900

    기본적인 타입 나열

--- ts-frist/frist.ts
+++ ts-frist/frist.ts
@@ -1,2 +1,18 @@
-let a = "hello";
-a = 123;
+// type
+const a: number = 5;
+const b: string = "aa";
+const c: boolean = false;
+const d: undefined = undefined;
+const e: null = null;
+const f: symbol = Symbol.for("abc");
+// const g: bigint = 100n;
+
+const arr: string[] = ["12", "a", "b"];
+const arr2: number[] = [1, 2, 3];
+const arr3: [number, number, string] = [1, 22, "a"];
+const obj: { lat: number; lon: number } = { lat: 37.5, lon: 127.5 };
+
+function addFuc(x: number, y: number): number {
+  return x + y;
+}
+const addArrow: (x: number, y: number) => number = (x, y) => x + y;
