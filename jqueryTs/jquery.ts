8b49390a4920f3b58361c0d93a39051ad558df2b
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-08-13 21:57:41 +0900

    jquery type 재정의

--- jqueryTs/jquery.ts
+++ jqueryTs/jquery.ts
@@ -1,5 +1,27 @@
+interface zQuery<T> {
+  text(
+    param?:
+      | string
+      | number
+      | ((this: T, index: number) => string | number | boolean)
+  ): this;
+  html(param: string | Document | DocumentFragment): void;
+}
+
+const $tag: zQuery<HTMLElement> = $([
+  "p",
+  "t",
+]) as unknown as zQuery<HTMLElement>;
+
 $("p").removeClass("myClass noClass").addClass("yourClass");
-$(["p", "t"]).text("hello");
+$tag.text("hello");
+$tag.text(123);
+$tag.text(function (index) {
+  console.log(this, index);
+  return true;
+});
+
+$tag.text().html(document);
 const tag = $("ul li")
   .addClass("hello")
   .addClass(function (idx) {
